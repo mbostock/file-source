c0d1c32c26359558f76dc564b6875a3548affe89
Mike Bostock <mbostock@gmail.com>
2016-09-02 10:27:21 -0700

    End operation on error.

--- source/read/index.js
+++ source/read/index.js
@@ -9,7 +9,7 @@ module.exports = function(length) {
     var source = this, buffer = new Buffer(length);
     (function read(offset) {
       fs.read(source._fd, buffer, offset, length - offset, source._position, function(error, length1) {
-        if (error) return reject(error);
+        if (error) return source._active = false, reject(error);
         if (!length1) return source._active = false, resolve(buffer.slice(0, offset));
         source._position += length1;
         if ((offset += length1) < length) return read(offset);
